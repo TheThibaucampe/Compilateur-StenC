91ec0b2ae3b74f34be81b41eb990c62b58a0c1b0
Victor <victor.constans@etu.unistra.fr>
2017-11-26 17:59:13 +0100

    Replace variable name by register into assembly code

--- src/tradCode.c
+++ src/tradCode.c
@@ -44,29 +44,40 @@ void tradCodeFinal(char* outputFileName, struct quads* quads,struct symbol* tds)
 
 		if(strcmp(curseur_quads->op,"j") == 0)
 		{
-			fprintf(outputFile,"%s %s\n",curseur_quads->op,curseur_quads->res->nom);
+			fprintf(outputFile,"j %s\n",curseur_quads->res->nom);
 		}
 
 		else if(strcmp(curseur_quads->op,"move") == 0)
 		{
-			fprintf(outputFile,"%s %s %s\n",curseur_quads->op, curseur_quads->res->nom,curseur_quads->arg1->nom);
+			fprintf(outputFile,"lw $t0 %s\n",curseur_quads->arg1->nom);
+			fprintf(outputFile,"move $t1 $t0\n");
+			fprintf(outputFile,"sw $t1 %s\n",curseur_quads->res->nom);
 		}
 
 		else if(strcmp(curseur_quads->op,"beq") == 0 ||strcmp(curseur_quads->op,"bne") == 0 ||strcmp(curseur_quads->op,"ble") == 0 ||strcmp(curseur_quads->op,"blt") == 0 ||strcmp(curseur_quads->op,"bge") == 0 ||strcmp(curseur_quads->op,"bgt") == 0)
 		{
-		
-			fprintf(outputFile,"%s %s %s %s\n",curseur_quads->op,curseur_quads->arg1->nom, curseur_quads->arg2->nom,curseur_quads->res->nom);
+			fprintf(outputFile,"lw $t0 %s\n",curseur_quads->arg1->nom);
+			fprintf(outputFile,"lw $t1 %s\n",curseur_quads->arg2->nom);
+			fprintf(outputFile,"%s $t0 $t1 %s\n",curseur_quads->op,curseur_quads->res->nom);
 		}
 
 		else
 		{
-			fprintf(outputFile,"%s %s %s %s\n",curseur_quads->op,curseur_quads->res->nom, curseur_quads->arg1->nom, curseur_quads->arg2->nom);
+			fprintf(outputFile,"lw $t0 %s\n",curseur_quads->arg1->nom);
+			fprintf(outputFile,"lw $t1 %s\n",curseur_quads->arg2->nom);
+			fprintf(outputFile,"%s $t2 $t0 $t1\n",curseur_quads->op);
+			fprintf(outputFile,"sw $t2 %s\n",curseur_quads->res->nom);
 		}
 
 		curseur_quads = curseur_quads->suivant;
 
 		instr_cmpt++;
 
+	}
+
+	if((label = lookup_label(tds,instr_cmpt)) != NULL)
+	{
+		fprintf(outputFile,"%s:\n",label->nom);
 	}
 
 	fclose(outputFile);
