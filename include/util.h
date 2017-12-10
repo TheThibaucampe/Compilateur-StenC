a3ae744ef3f18e6611b1dc1b6b7555fa71429521
TheThibaucampe <peyretthibaut@gmail.com>
2017-12-10 17:09:58 +0100

    Librairie des frees mise à jour

--- include/util.h
+++ include/util.h
@@ -4,6 +4,8 @@
 #include "tds.h"
 #include "quads.h"
 #include "list_quads.h"
+#include "listNumber.h"
+#include "enum.h"
 
 //enum type error
 
@@ -11,6 +13,7 @@ void print_error(int, char*);
 void free_tds(struct symbol*);
 void free_code(struct quads*);
 void free_list_quad(struct list_quads*);
+void free_listNumber(struct listNumber*);
 void free_all();
 
 #endif
